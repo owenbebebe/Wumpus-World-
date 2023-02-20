dc8d5a712a8d10310be4c25f98bc4a5c08f695ed
Owen Li <87036763+owenbebebe@users.noreply.github.com>
2023-02-20 00:33:59 -0500

    Add files via upload

--- js/board.js
+++ js/board.js
@@ -1,32 +1,114 @@
 export default class Board {
-    constructor() {
-        this.pos = [7,0];
-        this.brd = [
-            [[], [], [], [], [], [], [], []],
-            [[], [], [], [], [], [], [], []],
-            [[], [], [], [], [], [], [], []],
-            [[], [], [], [], [], [], [], []],
-            [[], [], [], [], [], [], [], []],
-            [[], [], [], [], [], [], [], []],
-            [[], [], [], [], [], [], [], []],
-            [['A'], [], [], [], [], [], [], []],
-        ]
+    constructor(num) {
+        this.size = num;
+        this.attack = 2;
+        this.face = 'E';
+        this.pos = [num-1,0];
+        this.brd = [];
+        this.dead = false;
     }
 
-    deleteA(r, c) {
-        for(var i = 0; i < brd[r][c].length; i++) {
-            if(brd[r][c][i] == 'A') {
-                brd[r][c].splice(i,1);
+    generateBrd() {
+        for ( let i = 0; i < this.size; i++) {
+            let temp = [];
+            for (let j = 0; j < this.size; j++) {
+                temp.push([]);
+            }
+            this.brd.push(temp);
+        }
+        this.brd[this.size-1][0].push('A');
+    }
+
+
+    deleteA(r, c, cc) {
+        for(var i = 0; i < this.brd[r][c].length; i++) {
+            if(this.brd[r][c][i] == cc) {
+                this.brd[r][c].splice(i,1);
             }
         }
     }
 
     movePosition(move) {
-        let i = this.position[0];
-        let j = this.position[1];
-        deleteA(i, j);
+        let i = this.pos[0];
+        let j = this.pos[1];
+        this.deleteA(i, j, 'A');
         this.brd[move[0]][move[1]].push('A');
         this.pos = move;
     }
-}
 
+    checkStatus() {
+        for (let i = 0; i < this.brd[this.pos[0]][this.pos[1]].length; i++) {
+            if (this.brd[this.pos[0]][this.pos[1]][i] === 'P' || this.brd[this.pos[0]][this.pos[1]][i] === 'W') {
+                let temp = document.querySelector('.gameover-box');
+                temp.style.display = 'flex';
+                this.dead = true;
+            }
+            else if (this.brd[this.pos[0]][this.pos[1]][i] === 'T') {
+                let temp = document.querySelector('.next-box');
+                temp.style.display = 'flex';
+                this.dead = true;
+            }
+        }
+    }
+
+
+    spawnPit(r, c) {
+        this.brd[r][c].push('P');
+        //check upper square
+        if (r - 1 != -1) {
+            this.brd[r-1][c].push('B');
+        }
+        //check for lower
+        if (r + 1 != this.size) {
+            this.brd[r+1][c].push('B');
+        }
+        //check for left
+        if (c - 1 != -1) {
+            this.brd[r][c-1].push('B');
+        }
+        //check for right
+        if (c + 1 != this.size) {
+            this.brd[r][c+1].push('B');
+        }
+    }
+
+    spawnWumpus(r, c){
+        this.brd[r][c].push('W');
+            //check upper square
+            if (r - 1 != -1) {
+                this.brd[r-1][c].push('S');
+            }
+            //check for lower
+            if (r + 1 != this.size) {
+                this.brd[r+1][c].push('S');
+            }
+            //check for left
+            if (c - 1 != -1) {
+                this.brd[r][c-1].push('S');
+            }
+            //check for right
+            if (c + 1 != this.size) {
+                this.brd[r][c+1].push('S');
+            }
+    }
+
+    spawnTreasure(r, c){
+        this.brd[r][c].push('T');
+            //check upper square
+            if (r - 1 != -1) {
+                this.brd[r-1][c].push('G');
+            }
+            //check for lower
+            if (r + 1 != this.size) {
+                this.brd[r+1][c].push('G');
+            }
+            //check for left
+            if (c - 1 != -1) {
+                this.brd[r][c-1].push('G');
+            }
+            //check for right
+            if (c + 1 != this.size) {
+                this.brd[r][c+1].push('G');
+            }
+    }
+}
--- js/script.js
+++ js/script.js
@@ -1,168 +1,196 @@
 // import Board module
-class Board {
-    constructor() {
-        this.face = 'E';
-        this.pos = [7,0];
-        this.brd = [
-            [[], [], [], [], [], [], [], []],
-            [[], [], [], [], [], [], [], []],
-            [[], [], [], [], [], [], [], []],
-            [[], [], [], [], [], [], [], []],
-            [[], [], [], [], [], [], [], []],
-            [[], [], [], [], [], [], [], []],
-            [[], [], [], [], [], [], [], []],
-            [['A'], [], [], [], [], [], [], []],
-        ]
-    }
+import Board from './board.js';
 
-    deleteA(r, c) {
-        for(var i = 0; i < this.brd[r][c].length; i++) {
-            if(this.brd[r][c][i] == 'A') {
-                this.brd[r][c].splice(i,1);
-            }
-        }
-    }
-
-    movePosition(move) {
-        let i = this.pos[0];
-        let j = this.pos[1];
-        this.deleteA(i, j);
-        this.brd[move[0]][move[1]].push('A');
-        this.pos = move;
-    }
-
-    checkStatus() {
-        for (let i = 0; i < this.brd[this.pos[0]][this.pos[1]].length; i++) {
-            if (this.brd[this.pos[0]][this.pos[1]][i] === 'P' || this.brd[this.pos[0]][this.pos[1]][i] === 'W') {
-                console.log("You are Dead");
-            }
-            else if (this.brd[this.pos[0]][this.pos[1]][i] === 'T') {
-                console.log("You found the glitter");
-            }
-        }
-    }
-
-
-    spawnPit(r, c) {
-        this.brd[r][c].push('P');
-        //check upper square
-        if (r - 1 != -1) {
-            this.brd[r-1][c].push('B');
-        }
-        //check for lower
-        if (r + 1 != 8) {
-            this.brd[r+1][c].push('B');
-        }
-        //check for left
-        if (c - 1 != -1) {
-            this.brd[r][c-1].push('B');
-        }
-        //check for right
-        if (c + 1 != 8) {
-            this.brd[r][c+1].push('B');
-        }
-    }
-
-    spawnWumpus(r, c){
-        this.brd[r][c].push('W');
-            //check upper square
-            if (r - 1 != -1) {
-                this.brd[r-1][c].push('S');
-            }
-            //check for lower
-            if (r + 1 != 8) {
-                this.brd[r+1][c].push('S');
-            }
-            //check for left
-            if (c - 1 != -1) {
-                this.brd[r][c-1].push('S');
-            }
-            //check for right
-            if (c + 1 != 8) {
-                this.brd[r][c+1].push('S');
-            }
-    }
-
-    spawnTreasure(r, c){
-        this.brd[r][c].push('T');
-            //check upper square
-            if (r - 1 != -1) {
-                this.brd[r-1][c].push('G');
-            }
-            //check for lower
-            if (r + 1 != 8) {
-                this.brd[r+1][c].push('G');
-            }
-            //check for left
-            if (c - 1 != -1) {
-                this.brd[r][c-1].push('G');
-            }
-            //check for right
-            if (c + 1 != 8) {
-                this.brd[r][c+1].push('G');
-            }
-    }
-}
-
-
-
-
+var grid_size = 9;
+var pit_size = grid_size-4;
+var wumpus_size = 1;
+var wumpus_pos = [];
 var gap = 5;
 var cellWidth = 60;
 var upper_layer;
+var fog_layer;
+var fog_board = generateFog(0);
+var jump = false;
 
-let board = new Board;
+if(grid_size > 7) {
+    jump = true;
+}
+
+if (grid_size >= 8) {
+    wumpus_size = grid_size-7;
+}
+
+let board = new Board(grid_size);
+
+board.generateBrd();
 
 window.onload = function () {
     upper_layer = document.querySelector(".upper_layer");
+    fog_layer = document.querySelector(".fog_layer");
+    let btn_next = document.querySelector(".next-level")
+    let btn_restart = document.querySelector(".restart")
+    btn_next.onclick = function(){
+        pit_size++;
+        if(pit_size == grid_size-2) {
+            grid_size++;
+            pit_size = grid_size-4;
+        }
+        if (grid_size >= 8) {
+            jump = true;
+            wumpus_size = grid_size-7;
+        }
+        board = new Board(grid_size);
+        board.generateBrd();
+        fog_board = generateFog(0);
+        wumpus_pos = [];
+        generateObj();
+        drawPieces();
+        drawfog();
+        drawBoard();
+        resizeBoard();
+        let temp = document.querySelector('.next-box');
+        temp.style.display = 'none';
+    }
+    btn_restart.onclick = function(){
+        board = new Board(grid_size);
+        board.generateBrd();
+        fog_board = generateFog(0);
+        wumpus_pos = [];
+        generateObj();
+        drawPieces();
+        drawfog();
+        drawBoard();
+        resizeBoard();
+        let temp = document.querySelector('.gameover-box');
+        temp.style.display = 'none';
+    }
+    resizeBoard();
     drawBoard(); 
     drawPieces();
+    drawfog();
+    generateObj();
 }
 
 
-// document.addEventListener("keydown", function(event) {
-//     if (event.code === "ArrowUp" && position[0] != 0) {
-        
-//     }
-//   });
+function generateFog(c) {
+    let ans = []
+    for(var i = 0; i < grid_size; i++) {
+        let temp = [];
+        for(var j = 0; j < grid_size; j++) {
+            temp.push(c); 
+        }
+        ans.push(temp);
+    }
+    ans[grid_size-1][0] = 1;
+    return ans;
+}
+
+
+function resizeBoard() {
+    let b = document.querySelector(".board-box");
+    let bb = document.querySelector(".board");
+    b.style.width=grid_size*65.9375 + "px";
+    b.style.height=grid_size*65.9375 + "px";
+    bb.style.width=grid_size*65.9375 + "px";
+    bb.style.height=grid_size*65.9375 + "px";
+}
+
 
 //print out the eight by eight grid
 function drawBoard() {
-    for(var i = 0; i < 8; i++) {
-        for(var j = 0; j < 8; j++) {
+    for(var i = 0; i < grid_size; i++) {
+        for(var j = 0; j < grid_size; j++) {
             let blk = document.createElement("div");
             blk.style.width = cellWidth+"px";
             blk.style.height = cellWidth+"px";
             blk.classList.add("block");
             blk.style.left = ((cellWidth + gap) * j + gap)+"px";
             blk.style.top = ((cellWidth + gap) * i + gap)+"px";
+            if(i == 0) {
+                if(j == 0) {
+                    blk.classList.add("block-corner");
+                    blk.style.transform="rotate(0deg)";
+                }
+                else if(j == grid_size-1) {
+                    blk.classList.add("block-corner");
+                    blk.style.transform="rotate(90deg)";
+                }
+                else {
+                    blk.classList.add("block-side");
+                }
+            }
+            else if(i ==  grid_size-1) {
+                if(j == 0) {
+                    blk.classList.add("block-corner");
+                    blk.style.transform="rotate(-90deg)";
+                }
+                else if(j ==  grid_size-1) {
+                    blk.classList.add("block-corner");
+                    blk.style.transform="rotate(180deg)";
+                }
+                else {
+                    blk.classList.add("block-side");
+                    blk.style.transform="rotate(180deg)";
+                }
+            }
+            else if(j == 0 && i !=  grid_size-1 && i != 0) {
+                blk.classList.add("block-side");
+                blk.style.transform="rotate(-90deg)";
+            }
+            else if(j ==  grid_size-1 && i !=  grid_size-1 && i != 0) {
+                blk.classList.add("block-side");
+                blk.style.transform="rotate(90deg)";
+            }
+            else {
+                let num = Math.floor(Math.random() * 2);
+                if(num == 0) blk.classList.add("block-fill1")
+                else  blk.classList.add("block-fill2");
+            }
             document.querySelector(".board").appendChild(blk);
         }
     }
 }
 
 
+
 function drawPieces() {
     upper_layer.innerHTML = "";
-    for(var i = 0; i < 8; i++) {
-        for(var j = 0; j < 8; j++) {
+    for(var i = 0; i < grid_size; i++) {
+        for(var j = 0; j < grid_size; j++) {
             if (board.brd[i][j] !== []) {
                 for(var k = 0; k < board.brd[i][j].length; k++) {
                     let piece = document.createElement("div");
-                    piece.style.width = cellWidth+"px";
-                    piece.style.height = cellWidth+"px";
-                    piece.style.left = ((cellWidth + gap) * j + gap + 2)+"px";
-                    piece.style.top = ((cellWidth + gap) * i + gap + 12)+"px";
                     if(board.brd[i][j][k] === 'A') {
-                        piece.style.backgroundColor = 'red';
-                        piece.classList.add("arrow");
+                        piece.style.top = ((cellWidth + gap) * i + gap -15)+"px";
+                        piece.style.left = ((cellWidth + gap) * j + gap + 10)+"px";
+                        piece.classList.add("player")
+                        piece.style.width = cellWidth-20+"px";
+                        piece.style.height = cellWidth+20+"px";
+                        if(board.face === 'U') {
+                            piece.style.backgroundImage="url(/images/player-back.png)";
+                        }
+                        if(board.face === 'D') {
+                            piece.style.backgroundImage="url(/images/player-front.png)";
+                        }
+                        if(board.face === 'L') {
+                            piece.style.backgroundImage="url(/images/player-left.png)";
+                        }
+                        if(board.face === 'R') {
+                            piece.style.backgroundImage="url(/images/player-side.png)";
+                        }
                     }
-                    if(board.brd[i][j][k] === 'P') piece.style.backgroundColor="black";
-                    if(board.brd[i][j][k] === 'B') piece.style.backgroundColor="blue";
-                    if(board.brd[i][j][k] === 'W') piece.style.backgroundColor="yellow";
-                    if(board.brd[i][j][k] === 'S') piece.style.backgroundColor="green";
-                    if(board.brd[i][j][k] === 'T') piece.style.backgroundColor="brown";
-                    if(board.brd[i][j][k] === 'G') piece.style.backgroundColor="white";
+                    else {
+                        piece.style.top = ((cellWidth + gap) * i + gap + 12)+"px";
+                        piece.style.left = ((cellWidth + gap) * j + gap + 2)+"px";
+                        piece.style.width = cellWidth+"px";
+                        piece.style.height = cellWidth+"px";
+                    }
+                    if(board.brd[i][j][k] === 'P') piece.classList.add("pit");
+                    if(board.brd[i][j][k] === 'B') piece.classList.add("breeze");
+                    if(board.brd[i][j][k] === 'W') piece.classList.add("wumpus");
+                    if(board.brd[i][j][k] === 'S') piece.classList.add("stench");
+                    if(board.brd[i][j][k] === 'T') piece.classList.add("treasure");
+                    if(board.brd[i][j][k] === 'G') piece.classList.add("glitter");
                     piece.classList.add("piece");
                     document.querySelector(".upper_layer").appendChild(piece);
                 }
@@ -172,46 +200,178 @@ function drawPieces() {
 }
 
 
-document.addEventListener("keydown", function(event) {
-    let player = document.querySelector('.arrow'); 
-    if (event.key === "w") {
-        board.face = 'U';
-        player.style.transform = "rotate(90deg)";
-    }
-    if (event.key === "s") {
-        board.face = 'D';
-        player.style.transform = "rotate(270deg)"; 
-
-    }
-    if (event.key === "a") {
-        board.face = 'L';
-        player.style.transform = "rotate(0deg)"; 
-
-    }
-    if (event.key === "d") {
-        board.face = 'R';
-        player.style.transform = "rotate(0deg)"; 
-    }
-
-    if (event.keyCode === 32) {
-        if (board.face === 'U') {
-            board.movePosition([board.pos[0]-1, board.pos[1]]);
-        }   
-        if (board.face === 'D') {
-            board.movePosition([board.pos[0]+1, board.pos[1]]);
-        }   
-        if (board.face === 'L') {
-            board.movePosition([board.pos[0], board.pos[1]-1]);
-        }   
-        if (board.face === 'R') {
-            board.movePosition([board.pos[0], board.pos[1]+1]);
+function drawfog() {
+    fog_layer.innerHTML = "";
+    for(var i = 0; i < grid_size; i++) {
+        for(var j = 0; j < grid_size; j++) {
+            if (fog_board[i][j] === 0) {
+                let fog = document.createElement("div");
+                fog.style.width = cellWidth+"px";
+                fog.style.height = cellWidth+"px";
+                fog.style.left = ((cellWidth + gap) * j + gap + 2)+"px";
+                fog.style.top = ((cellWidth + gap) * i + gap + 12)+"px";
+                fog.classList.add("fog");
+                document.querySelector(".fog_layer").appendChild(fog);
+            }
         }
     }
-    board.checkStatus();
-    drawPieces();
+}
+
+
+function updateattacks () {
+    attk = document.querySelector('.attack-num');
+    attk.textContent = board.attack;
+}
+
+function generateObj() {   
+    let t = 0;
+    let danger = [];
+    for(let i = 0; i < pit_size; i++) {
+        t = generateNum();
+        danger.push(t);
+        board.spawnPit(t[0], t[1]);
+    }
+    
+    for(let i = 0; i < wumpus_size; i++) {
+        t = generateNum();
+        danger.push(t);
+        board.spawnWumpus(t[0], t[1]);
+        wumpus_pos.push(t);
+    }
+    
+    t= generateNum();
+    let size = danger.length
+    let b = true
+    while (b == true) {
+        b = false;
+        for(let i = 0; i < size; i++) {
+            if (danger[i][0] === t[0] && danger[i][1] === t[1]) {
+                t= generateNum();
+                b = true;
+                break;
+            }
+        }
+    }
+    
+    board.spawnTreasure(t[0],t[1]);
+
+}
+
+function moveWumpus() {
+    for (let i = 0; i < wumpus_pos.length; i++) {
+        let wr = wumpus_pos[i][0];
+        let wc = wumpus_pos[i][1];
+        //delete wumpus
+        board.deleteA(wr, wc, 'W');
+        //delete breeze
+        //check upper square
+        let pos_moves = [];
+        if (wr - 1 != -1) {
+            board.deleteA(wr-1, wc, 'S');
+            let temp = [];
+            temp.push(wr-1);
+            temp.push(wc);
+            pos_moves.push(temp);
+        }
+        //check for lower
+        if (wr + 1 != grid_size) {
+            board.deleteA(wr+1, wc, 'S');
+            let temp = [];
+            temp.push(wr+1);
+            temp.push(wc);
+            pos_moves.push(temp);
+        }
+        //check for left
+        if (wc - 1 != -1) {
+            board.deleteA(wr, wc-1, 'S');
+            let temp = [];
+            temp.push(wr);
+            temp.push(wc-1);
+            pos_moves.push(temp);
+        }
+        //check for right
+        if (wc + 1 != grid_size) {
+            board.deleteA(wr, wc+1, 'S');
+            let temp = [];
+            temp.push(wr);
+            temp.push(wc+1);
+            pos_moves.push(temp);
+        }
+        //spawn the new wumpus
+        let r = Math.floor(Math.random() * pos_moves.length);
+        board.spawnWumpus(pos_moves[r][0], pos_moves[r][1]);
+        wumpus_pos[i] = pos_moves[r];
+    }
+}
+
+
+document.addEventListener("keydown", function(event) {
+    if(board.dead != true) {
+            let player = document.querySelector('.arrow'); 
+        if (event.key === "w") {
+            board.face = 'U';
+        }
+        if (event.key === "s") {
+            board.face = 'D';
+        }
+        if (event.key === "a") {
+            board.face = 'L';
+        }
+        if (event.key === "d") {
+            board.face = 'R';
+        }
+
+        if (event.keyCode === 32) {
+            if (board.face === 'U') {
+                if(board.pos[0] != 0) {
+                    board.movePosition([board.pos[0]-1, board.pos[1]]);
+                }
+                
+            }   
+            if (board.face === 'D') {
+                if(board.pos[0] !=  grid_size-1) {
+                    board.movePosition([board.pos[0]+1, board.pos[1]]);
+                }
+            }   
+            if (board.face === 'L') {
+                if(board.pos[1] != 0) {
+                    board.movePosition([board.pos[0], board.pos[1]-1]);
+                }
+            }   
+            if (board.face === 'R') {
+                if(board.pos[1] !=  grid_size-1) {
+                    board.movePosition([board.pos[0], board.pos[1]+1]);
+                }
+            }
+            //update the jumping effect too
+            if(jump == true) {
+                moveWumpus();
+            }
+            
+        }
+
+        if(event.key === 'z') {
+            board.attack--;
+            updateattacks();
+        }
+        fog_board[board.pos[0]][board.pos[1]] = 1;
+        board.checkStatus();
+        if(board.dead == true) {
+            fog_board = generateFog(1);
+        }
+        drawfog();
+        drawPieces();
+    }
+    
 });
 
-board.spawnPit(3,3);
-board.spawnPit(6,6);
-board.spawnWumpus(3,0);
-board.spawnTreasure(0,7);
+function generateNum() {
+    let randn1 = Math.floor(Math.random() * grid_size);
+    let randn2 = Math.floor(Math.random() * grid_size);
+    while((randn1 == grid_size-1 && randn2 == 0) || (randn1 == grid_size-2 && randn2 == 0)
+        || (randn1 == grid_size-1 && randn2 == 1) || (randn1 == grid_size-2 && randn2 == 1)) {
+        randn1 = Math.floor(Math.random() * grid_size);
+        randn2 = Math.floor(Math.random() * grid_size);
+    }
+    return [randn1, randn2];
+}
